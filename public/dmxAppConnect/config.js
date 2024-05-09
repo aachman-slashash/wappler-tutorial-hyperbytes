8eba883416f0a31aa085c22d6df2ea0d74949a1d
Aachman Mittal <aachman.mittal@slashash.co>
2024-05-09 14:55:29 +0530

    Saving Image details to DB and Showing cover image thumbs in upload image modal (uploaded images)
    VID 48 and 49
    (And a lots of images in public/uploads folder.....)

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,25 @@
+dmx.config({
+  "page_content": {
+    "image_display": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "Image_id"
+        },
+        {
+          "type": "number",
+          "name": "page_content_id"
+        },
+        {
+          "type": "text",
+          "name": "image_url"
+        },
+        {
+          "type": "number",
+          "name": "creator_id"
+        }
+      ],
+      "outputType": "array"
+    }
+  }
+});
